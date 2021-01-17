9f8b3093db099c1e0e99cbe0b43a288271111151
mirrorkeydev <mirrorkeydev@gmail.com>
2021-01-17 11:45:22 -0800

    Ported home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import About from "./pages/About"
+import Projects from "./pages/Projects"
+// import Contact from "./pages/Contact"
+import Home from "./pages/Home"
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <Router>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
-
-export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import { Link } from "react-router-dom";
+import { useEffect, useRef } from 'react';
+import './Home.css';
+import video from "../assets/desktop-background.mp4"
+
+export default function Home() {
+  const vid = useRef(null);
+
+  const fadein = (element) => {
+    var op = 0;
+    var timer = setInterval(() => {
+      if (op >= 1) clearInterval(timer);
+      element.style.opacity = op;
+      element.style.filter = 'alpha(opacity=' + op * 100 + ')';
+      op += op * 0.1 || 0.1;
+    }, 50);
+    element.oncanplay = undefined;
+  }
+
+  useEffect(() => {
+    vid.current.oncanplay = () => {
+      console.log("hit!")
+      fadein(vid.current);
+    };
+  })
+
+  return (
+    <div className="home">
+      <video autoPlay loop muted playsInline id="home-video" ref={vid} className="blur">
+        <source src={video} type="video/mp4"/>
+      </video>
+      <nav id="nav-container">
+        <Link to="/about" className="nav-link">about</Link>
+        <Link to="/projects" className="nav-link">projects</Link>
+        <Link to="/contact" className="nav-link">contact</Link>
+      </nav>
+      <h1 id="title" className="unselectable">melanie<br/>gutzmann</h1>
+    </div>
+  )
+}
