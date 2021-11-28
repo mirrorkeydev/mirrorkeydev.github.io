e23805c4095203b590629771f4e72e6bf602242e
mirrorkeydev <mirrorkeydev@gmail.com>
2021-11-28 12:18:25 -0800

    Fix FOUT on home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,36 +5,41 @@ import video from "../assets/desktop-background.mp4"
 
 export default function Home() {
   const vid = useRef(null)
+  const title = useRef(null)
+  const nav = useRef(null)
 
-  const fadein = (element) => {
+  const fadein = (element, speed) => {
     var op = 0;
     var timer = setInterval(() => {
       if (op >= 1) clearInterval(timer);
       element.style.opacity = op;
       element.style.filter = 'alpha(opacity=' + op * 100 + ')';
       op += op * 0.1 || 0.1;
-    }, 50);
+    }, speed);
     element.oncanplay = undefined;
   }
 
   useEffect(() => {
     vid.current.oncanplay = () => {
-      fadein(vid.current);
+      fadein(vid.current, 50);
     };
+    // Wait for font load to avoid Flash Of Unstyled Text
+    document.fonts.load("12px Rubik").then(() => {fadein(title.current, 5)});
+    document.fonts.load("12px Roboto").then(() => {fadein(nav.current, 5)})
   }, [])
 
   return (
     <div className="home">
-      <video autoPlay loop muted playsInline id="home-video" ref={vid} className="blur" style={{opacity: 0}}>
+      <video autoPlay loop muted playsInline id="home-video" ref={vid} className="blur">
         <source src={video} type="video/mp4"/>
       </video>
-      <nav id="nav-container">
+      <nav id="nav-container" ref={nav}>
         <Link to="/about" className="nav-link">about</Link>
         <Link to="/projects" className="nav-link">projects</Link>
         <Link to="/art" className="nav-link">art</Link>
         <Link to="/contact" className="nav-link">contact</Link>
       </nav>
-      <h1 id="title" className="unselectable">melanie<br/>gutzmann</h1>
+      <h1 id="title" className="unselectable" ref={title}>melanie<br/>gutzmann</h1>
     </div>
   )
 }
