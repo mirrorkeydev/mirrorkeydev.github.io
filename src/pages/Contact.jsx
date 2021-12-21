274303a345d8a7a46842bdf5373fe5b95ea8ed2e
Melanie <mirrorkeydev@gmail.com>
2021-12-21 13:10:13 -0800

    Update project and contact info

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -8,7 +8,7 @@ export default function Contact() {
         <FadeIn>
           <div><a href="mailto:mirrorkeydev@gmail.com"><span></span>mirrorkeydev<span>@gmail.com</span></a></div>
           <div><a href="https://github.com/mirrorkeydev"><span>github.com/</span>mirrorkeydev<span></span></a></div>
-          <div><a href="https://discord.com/"><span></span>mirrorkeydev<span>#0120</span></a></div>
+          <div><a href="https://discord.com/users/384231172575592458/"><span></span>mirrorkeydev<span>#0120</span></a></div>
           <div><a href="https://www.linkedin.com/in/mirrorkeydev/"><span>linkedin.com/in/</span>mirrorkeydev<span></span></a></div>
         </FadeIn>
       </div>
