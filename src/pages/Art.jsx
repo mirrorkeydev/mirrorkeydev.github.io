e6cbbd6cf9f50382adc62255d967bda6b6b6c58a
mirrorkeydev <mirrorkeydev@gmail.com>
2022-02-19 12:15:58 -0800

    Add art timelapses on hover

--- src/pages/Art.jsx
+++ src/pages/Art.jsx
@@ -3,6 +3,16 @@ import imageData from '../assets/pig.json'
 
 export default function Art() {
   return (
-    <Pig imageData={imageData} />
+    <Pig
+      imageData={imageData}
+      getUrl={(url, pxHeight) => {
+        // I don't have cloudinary premium and therefore cannot
+        // access large gifs. Clamp down to h_400. Sad 😔
+        if (url.includes('.gif') && pxHeight > 400){
+          return url.replace('{{HEIGHT}}', 400)
+        }
+        return url.replace('{{HEIGHT}}', pxHeight)
+      }}
+    />
   )
 }
